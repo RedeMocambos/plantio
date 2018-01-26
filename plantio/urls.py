252445f46cf231ff5b1df0bf24361c47ec7fe351
fernao <fernao@riseup.net>
2018-01-26 00:26:00 -0200

    [Update] Atualizando para django 2.0

--- plantio/urls.py
+++ plantio/urls.py
@@ -1,17 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.urls import path
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'plantio.views.home', name='home'),
-    # url(r'^plantio/', include('plantio.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
