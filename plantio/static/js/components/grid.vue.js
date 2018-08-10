04da19481f490a9343cdd0ff2a99fac0a478a861
fernao <fernao@riseup.net>
2018-08-10 04:27:14 -0300

    [ADD] Função para desenho de círculos - midpoint circle algorithm

--- plantio/static/js/components/grid.vue.js
+++ plantio/static/js/components/grid.vue.js
@@ -17,6 +17,15 @@ Vue.component('grid', {
           <div class="input-field col s12">
             <a class="waves-effect waves-light btn" v-on:click="gerarGrid()">gerar grid</a>
           </div>
+
+
+          <div class="input-field col s3">
+            <input placeholder="Raio" id="raio" type="text" class="validate" v-model="opt.raio">
+            <label for="raio">Raio</label>
+          </div>
+          <div class="input-field col s12">
+            <a class="waves-effect waves-light btn" v-on:click="drawCircle()">gerar circulo</a>
+          </div>
         </div>
       </div>
     </div>
@@ -38,64 +47,130 @@ Vue.component('grid', {
     props: {
     },
     data: function() {
-	return {
-	    opt: {
-		sizeX: 3,
-		sizeY: 3,
-		boxSizeW: 25,
-		boxSizeH: 25
-	    },
-	    gridData: [
-		[] = '0'
-	    ],
-	    scale: {
-		'0': '',
-		'1': 'light-green lighten-4',
-		'2': 'light-green lighten-3',
-		'3': 'green lighten-3',
-		'4': 'green lighten-2',
-		'5': 'green lighten-1',
-		'6': 'green',
-		'7': 'green darken-1',
-		'8': 'green darken-2',
-		'9': 'green darken-3',
-		'10': 'green darken-4'
-	    },
-	    iniciado: false,
-	    output: ''
-	}
+        return {
+            opt: {
+                sizeX: 20,
+                sizeY: 20,
+                boxSizeW: 15,
+                boxSizeH: 15,
+                raio: 9,
+            },
+            gridData: [
+                [] = '0'
+            ],
+            scale: {
+                '0': '',
+                '1': 'light-green lighten-4',
+                '2': 'light-green lighten-3',
+                '3': 'green lighten-3',
+                '4': 'green lighten-2',
+                '5': 'green lighten-1',
+                '6': 'green',
+                '7': 'green darken-1',
+                '8': 'green darken-2',
+                '9': 'green darken-3',
+                '10': 'lime darken-1',
+                '11': 'lime darken-2',
+                '12': 'lime darken-3',
+                '13': 'lime darken-4',
+                '14': 'lime darken-5',
+
+                
+            },
+            iniciado: false,
+            output: ''
+        }
     },    
     created: function() {
     },
     methods: {
-	gerarGrid: function() {
-	    if (this.opt.sizeX != '' && this.opt.sizeY != '') {
-		this.gridData = this.generateGrid();
-		this.iniciado = true;
-	    }
-	},
-	
-	generateGrid: function() {
-	    let grid = [];
-	    this.opt.sizeX = parseInt(this.opt.sizeX);
-	    this.opt.sizeY = parseInt(this.opt.sizeY);    
-	    
-	    let xIn, xY = 0;
-	    for (let x = 0; x < this.opt.sizeX; x++) {
-		xIn = (Math.round(this.opt.sizeX /2) * -1) + x + 1;
+        gerarGrid: function() {
+            if (this.opt.sizeX != '' && this.opt.sizeY != '') {
+                this.gridData = this.generateGrid();
+                this.iniciado = true;
+            }
+        },
+        
+        generateGrid: function() {
+            let grid = [];
+            this.opt.sizeX = parseInt(this.opt.sizeX);
+            this.opt.sizeY = parseInt(this.opt.sizeY); 
 
-		grid[x] = [];
-		for (let y = 0; y < this.opt.sizeY; y++) {
-		    yIn = (Math.round(this.opt.sizeY /2) * -1) + y + 1;
+            let xIn, xY = 0;
+            for (let x = 0; x < this.opt.sizeX; x++) {
+                xIn = (Math.round(this.opt.sizeX /2) * -1) + x + 1;
 
-		    grid[x][y] = {
-			'x': xIn,
-			'y': yIn,
-			'value': 0
-		    }
-		}
-	    }
-	    return grid;
-	}
+                grid[x] = [];
+                for (let y = 0; y < this.opt.sizeY; y++) {
+                    yIn = (Math.round(this.opt.sizeY /2) * -1) + y + 1;
+
+                    let cor = 0;
+                    
+                    grid[x][y] = {
+                        'x': xIn,
+                        'y': yIn,
+                        'value': cor
+                    }
+                }
+            }
+            return grid;
+        },
+
+        drawCircle: function() {
+
+            this.opt.raio = parseInt(this.opt.raio);
+            this.opt.sizeX = parseInt(this.opt.sizeX);
+            
+            let raio = this.opt.raio;
+            
+             (raio >= (this.opt.sizeX / 2)) {
+                raio = parseInt((this.opt.sizeX / 2)) - 1;
+                this.opt.raio = raio;
+             }
+            
+            // Para fazer elipses, modular raioX e raioY. Para circulo perfeito, = raio
+            let raioX = raio;
+            let raioY = raio;
+            
+            let gridCircle = this.gridData;
+            
+
+            // Midpoint circle algorithm
+            // https://rosettacode.org/wiki/Bitmap/Midpoint_circle_algorithm
+            
+            let f = 1 - raio;
+            let ddf_x = 1
+            let ddf_y = -2 * raio;
+            let x = 0;
+            let y = raio;
+            let cor = 4;
+            
+            gridCircle[raioX][raioY + raio].value = cor;
+            gridCircle[raioX][raioY - raio].value = cor;
+            gridCircle[raioX + raio][raioY].value = cor;
+            gridCircle[raioX - raio][raioY].value = cor;
+
+            while (x < y) {
+                if (f >= 0) {
+                    y -= 1;
+                    ddf_y += 2;
+                    f += ddf_y;
+                }
+                x += 1;
+                ddf_x += 2;
+                f += ddf_x;
+                
+                gridCircle[raioX + x][raioY + y].value = cor;
+                gridCircle[raioX - x][raioY + y].value = cor;
+                gridCircle[raioX + x][raioY - y].value = cor;
+                gridCircle[raioX - x][raioY - y].value = cor;
+                gridCircle[raioX + y][raioY + x].value = cor;
+                gridCircle[raioX - y][raioY + x].value = cor;
+                gridCircle[raioX + y][raioY - x].value = cor;
+                gridCircle[raioX - y][raioY - x].value = cor;
+            }
+            
+            this.gridData  = gridCircle;            
+        }
     }
 })
