26ac44f82c78c04d4d349baab20da1e169b87a5e
fernao <fernao@riseup.net>
2018-09-04 01:53:40 -0300

    [ADD] API: Adicionando rota de listar áreas por localidade

--- app/area/urls.py
+++ app/area/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AreaViewSet, LocalidadeViewSet
 
 router = DefaultRouter()
+router.register(u'localidade/:id/areas', LocalidadeViewSet, base_name='lista_areas')
 router.register(u'localidade', LocalidadeViewSet, base_name='localidades')
 router.register(u'areas', AreaViewSet, base_name='areas')
 
--- app/area/views.py
+++ app/area/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Area, Localidade
 from .serializers import AreaSerializer, LocalidadeSerializer
@@ -13,3 +15,8 @@ class LocalidadeViewSet(ModelViewSet):
     queryset = Localidade.objects.all()
     serializer_class = LocalidadeSerializer
 
+    @action(methods=['get'], detail=True)
+    def areas(self, request, pk=True):
+        areas = Area.objects.filter(localidade=pk)
+        serializer = AreaSerializer(areas, many=True)
+        return Response(serializer.data)
