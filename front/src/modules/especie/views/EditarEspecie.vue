3fbbfeb0c78757b69c9cd8409a804ab7b9e04465
fernao <fernao@riseup.net>
2019-05-30 23:46:51 -0300

    [UPDATE] Front: edição de espécie #15

--- front/src/modules/especie/views/EditarEspecie.vue
+++ front/src/modules/especie/views/EditarEspecie.vue
@@ -1,118 +1,128 @@
 <template>
-    <v-form
-        v-model="valid"
+    <v-container
+        fluid
     >
-        <v-flex v-if="dadosEspecie.id === 'undefined'">
-            <carregando
-                :text="'Carregando dados da edição'"
-            />
-        </v-flex>
-        <v-flex
-            v-else
-            xs10
-            offset-xs1
-        >
-            <h3>Edição de espécie</h3>
-            <v-card
-                class="pa-3"
+        <v-layout>
+            <v-flex v-if="dadosEspecie.id === 'undefined'">
+                <carregando
+                    :text="'Carregando dados da edição'"
+                />
+            </v-flex>
+            <v-flex
+                v-else
+                xs10
+                offset-xs1
             >
-                <v-text-field
-                    v-model="dadosEspecie.familia"
-                    @change="atualizarCampo('familia', $event) "
-                    label="Família"
-                />
-                <v-text-field
-                    v-model="dadosEspecie.nome_cientifico"
-                    @change="atualizarCampo('nome_cientifico', $event) "
-                    label="Nome científico"
-                />
-                <v-text-field
-                    v-model="dadosEspecie.nomes_populares"
-                    @change="atualizarCampo('nomes_populares', $event) "
-                    label="Nomes populares"
-                />
-            </v-card>
-            <v-card
-                class="pa-3"
-            >
-                <v-text-field
-                    v-model="dadosEspecie.inicio_colheita"
-                    @change="atualizarCampo('inicio_colheita', $event) "
-                    label="Início da colheita"
-                />
-                <v-select
-                    v-model="dadosEspecie.porte"
-                    :items="getEspecieMetadata.porte"
-                    item-text="descricao"
-                    item-value="valor"
-                    @change="atualizarCampo('porte', $event) "
-                    label="Porte"
-                />
-                <v-select
-                    v-model="dadosEspecie.estrato"
-                    :items="getEspecieMetadata.estrato"
-                    item-text="descricao"
-                    item-value="valor"
-                    @change="atualizarCampo('estrato', $event) "
-                    label="Estrato"
-                />
-                <v-select
-                    v-model="dadosEspecie.sucessao"
-                    :items="getEspecieMetadata.sucessao"
-                    item-text="descricao"
-                    item-value="valor"
-                    @change="atualizarCampo('sucessao', $event) "
-                    label="Sucessão"
-                />
-                <v-select
-                    v-model="dadosEspecie.umidade"
-                    :items="getEspecieMetadata.umidade"
-                    @change="atualizarCampo('umidade', $event) "
-                    label="Umidade"
-                />
-                <v-select
-                    v-model="dadosEspecie.tolerancia_poda"
-                    :items="getEspecieMetadata.tolerancia_poda"
-                    @change="atualizarCampo('tolerancia_poda', $event) "
-                    label="Tolerância a poda"
-                />
-            </v-card>
-            <v-card
-                class="pa-3"
-            >
-                <v-text-field
-                    v-model="dadosEspecie.temperatura_min"
-                    xs4
-                    @change="atualizarCampo('temperatura_min', $event) "
-                    label="Temperatura mínima"
-                />
-                <v-text-field
-                    v-model="dadosEspecie.temperatura_max"
-                    xs4
-                    @change="atualizarCampo('temperatura_max', $event) "
-                    label="Temperatura máxima"
-                />
-            </v-card>
-            <v-btn
-                color="info"
-                @click="salvar()"
-            >
-                <v-icon
-                    class="pr-3"
-                >save</v-icon>
-                salvar
-            </v-btn>
-            <v-btn
-                color="error"
-                @click="voltar()"
-            >
-                <v-icon
-                    class="pr-3"
-                >cancel</v-icon>
-                cancelar
-            </v-btn>
-        </v-flex>
-    </v-form>
+                <v-form
+                    v-model="valid"
+                >
+                    <v-card
+                        class="pl-3"
+                    >
+                        <v-card-title>
+                            <div class="headline">
+                                Edição de espécie
+                            </div>
+                        </v-card-title>
+                        <v-text-field
+                            v-model="dadosEspecie.familia"
+                            label="Família"
+                            @change="atualizarCampo('familia', $event) "
+                        />
+                        <v-text-field
+                            v-model="dadosEspecie.nome_cientifico"
+                            label="Nome científico"
+                            @change="atualizarCampo('nome_cientifico', $event) "
+                        />
+                        <v-text-field
+                            v-model="dadosEspecie.nomes_populares"
+                            label="Nomes populares"
+                            @change="atualizarCampo('nomes_populares', $event) "
+                        />
+                    </v-card>
+                    <v-card
+                        class="pa-3"
+                    >
+                        <v-text-field
+                            v-model="dadosEspecie.inicio_colheita"
+                            label="Início da colheita"
+                            @change="atualizarCampo('inicio_colheita', $event) "
+                        />
+                        <v-select
+                            v-model="dadosEspecie.porte"
+                            :items="getEspecieMetadata.porte"
+                            item-text="descricao"
+                            item-value="valor"
+                            label="Porte"
+                            @change="atualizarCampo('porte', $event) "
+                        />
+                        <v-select
+                            v-model="dadosEspecie.estrato"
+                            :items="getEspecieMetadata.estrato"
+                            item-text="descricao"
+                            item-value="valor"
+                            label="Estrato"
+                            @change="atualizarCampo('estrato', $event) "
+                        />
+                        <v-select
+                            v-model="dadosEspecie.sucessao"
+                            :items="getEspecieMetadata.sucessao"
+                            item-text="descricao"
+                            item-value="valor"
+                            label="Sucessão"
+                            @change="atualizarCampo('sucessao', $event) "
+                        />
+                        <v-select
+                            v-model="dadosEspecie.umidade"
+                            :items="getEspecieMetadata.umidade"
+                            label="Umidade"
+                            @change="atualizarCampo('umidade', $event) "
+                        />
+                        <v-select
+                            v-model="dadosEspecie.tolerancia_poda"
+                            :items="getEspecieMetadata.tolerancia_poda"
+                            label="Tolerância a poda"
+                            @change="atualizarCampo('tolerancia_poda', $event) "
+                        />
+                    </v-card>
+                    <v-card
+                        class="pa-3"
+                    >
+                        <v-text-field
+                            v-model="dadosEspecie.temperatura_min"
+                            xs4
+                            label="Temperatura mínima"
+                            @change="atualizarCampo('temperatura_min', $event) "
+                        />
+                        <v-text-field
+                            v-model="dadosEspecie.temperatura_max"
+                            xs4
+                            label="Temperatura máxima"
+                            @change="atualizarCampo('temperatura_max', $event) "
+                        />
+                    </v-card>
+                    <v-btn
+                        color="info"
+                        @click="salvar()"
+                    >
+                        <v-icon
+                            class="pr-3"
+                        >save</v-icon>
+                        salvar
+                    </v-btn>
+                    <v-btn
+                        color="error"
+                        @click="voltar()"
+                    >
+                        <v-icon
+                            class="pr-3"
+                        >cancel</v-icon>
+                        cancelar
+                    </v-btn>
+                </v-form>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 import Carregando from '@/components/CarregandoVuetify';
